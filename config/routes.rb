0e68663b2934f1dbe26e6f1d23e37d622e5b4765
Cleyton Silva <silva.rcleyton@gmail.com>
2021-03-07 15:08:16 -0300

    Adjusting layout system

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
@@ -7,5 +10,7 @@ Rails.application.routes.draw do
   end
   devise_for :profiles
   devise_for :admins
-  root to: "home#index"
+  
+  get 'inicio', to: 'site/welcome#index'
+  root to: 'site/welcome#index'
 end
