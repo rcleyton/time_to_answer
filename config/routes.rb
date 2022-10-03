0a7d4c9fce9c2bc7b91b90abb380d81c1c11b4ee
Cleyton Silva <silva.rcleyton@gmail.com>
2022-10-03 10:23:27 -0300

    Create layout system to admins and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+  
   root to: 'welcome#index'
   get 'welcome/index'
 
