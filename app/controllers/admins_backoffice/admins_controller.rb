493cfb435df85bb467a9d4c0083eb0a756480052
Cleyton Silva <silva.rcleyton@gmail.com>
2022-10-08 11:24:44 -0300

    edit administrators

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -2,4 +2,22 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def index
     @admins = Admin.all
   end
+
+  def edit
+    @admin = Admin.find(params[:id])
+  end
+
+  def update
+    @admin = Admin.find(params[:id])
+    if @admin.update(admin_params)
+      redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso"
+    else  
+      render :edit
+    end
+  end
+
+  private
+  def admin_params
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :admins_backoffice do
-    get 'admins/index'
-  end
   namespace :site do
     get 'welcome/index'
   end
@@ -13,6 +10,7 @@ Rails.application.routes.draw do
 
   namespace :admins_backoffice do
     get 'welcome/index'
+    resources :admins, only: %i[ index edit update ]
   end
   
   devise_for :users
