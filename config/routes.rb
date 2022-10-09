a6c8c006d2ddd4c32bec9aee273e39b6c309535f
Cleyton Silva <silva.rcleyton@gmail.com>
2022-10-09 11:03:38 -0300

    delete administrator

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :admins_backoffice do
     get 'welcome/index'
-    resources :admins, only: %i[ index new create edit update ]
+    resources :admins
   end
   
   devise_for :users
