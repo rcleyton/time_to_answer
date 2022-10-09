07be25453a1c00515bd8e25e0957d1a750efa653
Cleyton Silva <silva.rcleyton@gmail.com>
2022-10-09 09:47:14 -0300

    change admin password

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,14 +1,15 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action :verify_password, only: [:update]
+  before_action :set_admin, only: %i[ edit update]
+
   def index
     @admins = Admin.all
   end
 
-  def edit
-    @admin = Admin.find(params[:id])
+  def edit 
   end
 
   def update
-    @admin = Admin.find(params[:id])
     if @admin.update(admin_params)
       redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso"
     else  
@@ -17,6 +18,16 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   end
 
   private
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+
+  def verify_password
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
+    end
+  end
+
   def admin_params
     params.require(:admin).permit(:email, :password, :password_confirmation)
   end
