1258be2e42a74d928787b1f1e6a19167612e253e
Cleyton Silva <silva.rcleyton@gmail.com>
2022-10-10 14:41:54 -0300

    create controllers and models to subjects

---
+++ app/controllers/admins_backoffice/subjects_controller.rb
@@ -0,0 +1,49 @@
+class AdminsBackoffice::SubjectsController < AdminsBackofficeController
+  before_action :set_subject, only: %i[ edit update destroy ]
+
+  def index
+    @subjects = Subject.all.page(params[:page])
+  end
+
+  def new
+    @subject = Subject.new
+  end
+
+  def create
+    @subject = Subject.create(subject_params)
+    if @subject.saver
+      redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área cadastrado com sucesso!"
+    else  
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @subject.update(subject_params)
+      redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área atualizado com sucesso!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @subject.destroy
+      redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área exclu´ido com sucesso!"
+    else
+      render :index
+    end
+  end
+
+  private
+  def set_subject
+    @subject = Subject.find(params[:id])
+  end
+
+  def subject_params
+    params.require(:subject).permit(:description)
+  end
+
+end
